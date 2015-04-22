b8ccd7babbe501b1046c3aae4f469f9e8dfd93bd
lalogf <lalogf@gmail.com>
2015-04-22 10:59:54 -0500

    changing the hamburger menu

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var colors = ["rgba(52, 152, 219, 0.6)","rgba(155, 89, 182,0.5)","rgba(230, 126, 34,0.5)",
-"rgba(52, 152, 45, 0.5)","rgba(155, 200, 182,0.5)","rgba(0, 126, 34,0.5)"]
+var colors = ["rgba(52, 152, 219, 0.6)","rgba(0, 26, 34,0.5)","rgba(230, 126, 34,0.5)",
+"rgba(52, 152, 45, 0.5)","rgba(155, 200, 182,0.5)","rgba(155, 89, 182,0.5)"]
 var arr = []
 
 var ready = function (){
@@ -10,6 +10,8 @@ var ready = function (){
 		$(arr).each(function(index){
 			$(this).attr("style","background-color:"+colors[index])
 		});
+	});
+	$(".navbar-toggle").focus(function(){
 	});
 };
 
