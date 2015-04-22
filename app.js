100470a301a5813de7f9f314089f7dbe438519ed
lalogf <lalogf@gmail.com>
2015-04-22 09:38:27 -0500

    Trying expandable navabar

---
+++ app.js
@@ -0,0 +1,17 @@
+var colors = ["rgba(52, 152, 219, 0.6)","rgba(155, 89, 182,0.5)","rgba(230, 126, 34,0.5)",
+"rgba(52, 152, 45, 0.5)","rgba(155, 200, 182,0.5)","rgba(0, 126, 34,0.5)"]
+var arr = []
+
+var ready = function (){
+	$(".bigbox").each(function(index) {
+		arr.push(this)
+	});
+	$(".navbar-toggle").click(function(){
+		$(arr).each(function(index){
+			$(this).attr("style","background-color:"+colors[index])
+		});
+	});
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
