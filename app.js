d6bcbfc712ac3ef98ca46017a0d430ebb136afde
lalogf <lalogf@gmail.com>
2015-04-23 17:11:15 -0500

    added animations

--- app.js
+++ app.js
@@ -7,12 +7,43 @@ var ready = function (){
 		arr.push(this)
 	});
 	$(".navbar-toggle").click(function(){
+		$(".icon-bar").each(function(index){
+			if (index === 0) {
+				if ($(this).hasClass('xone')){
+					$(this).removeClass('xone');
+				} else {
+					$(this).addClass('xone');	
+				}
+
+			} 
+			else if (index === 1) {
+				if($(this).hasClass('xtwo')) {
+					$(this).removeClass('xtwo');
+				} else {
+					$(this).addClass('xtwo');
+				}
+
+			} 
+			else if (index === 2) {
+				if ($(this).hasClass('xnone')){
+					$(this).attr("style", "background-color:white").removeClass('xnone')
+				} else {
+					$(this).attr("style", "background:none").addClass('xnone')
+				}
+			};
+		});
+		// $(":nth-child(3)", this).addClass('xtwo');
+		
+		// $(".navbar-toggle:nth-child(3)").attr('class','xtwo');
+		// $(".navbar-toggle:nth-child(4)").attr('class','xnone');
 		$(arr).each(function(index){
 			$(this).attr("style","background-color:"+colors[index])
 		});
 	});
 	$(".navbar-toggle").focus(function(){
 	});
+
+
 };
 
 $(document).ready(ready);
